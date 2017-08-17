bb71877d5ec731c04dbcce6cf519862470acdb33
Michael Foong <mikefoong@rawfactor.net>
2017-08-17 10:52:52 +0000

    Added GET Functionality

--- headlines2.py
+++ headlines2.py
@@ -13,11 +13,12 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-def index():
-  return redirect('http://flask.rawfactor.net/bbc', code=302)
-
-@app.route("/<publication>")
-def get_news(publication):
+def get_news():
+  query = request.args.get('publication')
+  if not query or query.lower() not in RSS_FEEDS:
+    publication = 'bbc'
+  else:
+    publication = query.lower()
   feed = feedparser.parse(RSS_FEEDS[publication])
   return render_template("home.html", articles=feed['entries'])
 
